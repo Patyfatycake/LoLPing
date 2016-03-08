ed940d344c5639d71c36cf6c587646cfb217044f
Mayusor <mayuso666@gmail.com>
2016-03-08 15:38:28 +0100

    Deleted unecessary extra code.

--- src/main/java/mayuso/LoLPing/Main.java
+++ src/main/java/mayuso/LoLPing/Main.java
@@ -7,33 +7,27 @@ public class Main {
 
 	public static void main(String[] args) {
 		try {
-			try {
-				for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-					if ("Nimbus".equals(info.getName())) {
-						UIManager.setLookAndFeel(info.getClassName());
-						break;
-					}
-				}
-				/*
-				 * try { UIManager.setLookAndFeel(UIManager.
-				 * getSystemLookAndFeelClassName()); } catch (Exception e) {
-				 * e.printStackTrace(); }
-				 */
-			} catch (Exception e) {
-				// If Nimbus is not available, fall back to cross-platform
-				try {
-					UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
-				} catch (Exception ex) {
-					
+
+			for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+				if ("Nimbus".equals(info.getName())) {
+					UIManager.setLookAndFeel(info.getClassName());
+					break;
 				}
 			}
+			/*
+			 * try { UIManager.setLookAndFeel(UIManager.
+			 * getSystemLookAndFeelClassName()); } catch (Exception e) {
+			 * e.printStackTrace(); }
+			 */
 		} catch (Exception e) {
+			// If Nimbus is not available, fall back to cross-platform
 			try {
 				UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
 			} catch (Exception ex) {
-				// not worth my time
+
 			}
 		}
+
 		new UI();
 	}
 
